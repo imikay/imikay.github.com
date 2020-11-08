90e29785ee6232a4fa7bc35ba9088e41c930884c
Mikay Gao <imikay@gmail.com>
2020-11-08 23:07:17 +0800

    Update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 $(function () {
   draw();
+
 });
 
-function draw(tree) {
+function draw() {
   
 }
+
+SVG.on(document, 'DOMContentLoaded', function() {
+  var draw = SVG().addTo('#canvas');
+  draw.rect(100,100).animate().fill('#f03').move(100,100);
+})
 
 
 
