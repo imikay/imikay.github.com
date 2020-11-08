018d3ab59d8b22eee1a9937c49b39ffc8616a5ee
Mikay Gao <imikay@gmail.com>
2020-11-09 00:15:26 +0800

    Update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
-$(function () {
-  draw();
-
-});
-
-function draw() {
-  
-}
-
 SVG.on(document, 'DOMContentLoaded', function() {
   var draw = SVG().addTo('#canvas');
-  draw.rect(100,100).animate().fill('#f03').move(100,100);
+  draw.circle(100);
+  draw.line(0, 100, 100, 0).move(20, 20);
+  line.stroke({ color: '#f06', width: 10, linecap: 'round' })
 })
-
-
-
